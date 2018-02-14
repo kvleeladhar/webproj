a392e1f8d586cacdde1184b27d8f656002f22556
devops <kvleeladhar@gmail.com>
2018-02-14 23:58:58 +0530

    deleted

--- proj-2/target/classes/META-INF/routing.js
+++
@@ -1,5 +0,0 @@
-// URL routing of the application.
-// routing = new Routing()
-// routing.ignore('*.js', '*.css', '*.gif', '*.png', '*.jpg', '*.jsp', '*.html')
-// routing.add('{user}/hello', Packages.com.example.your.app.Endpoint1, 'sayHello')
-// routing.add(Verb.GET, '{user}/goodbye', Packages.com.example.your.app.Endpoint2, 'sayGoodbye')
--- proj-2/target/proj-2-snap_2/WEB-INF/classes/META-INF/components.js
+++
@@ -1,5 +0,0 @@
-// Components to be deployed on the application (including web endpoints).
-// components = [
-//     Packages.com.example.your.app.Component1, 
-//     Packages.com.example.your.app.Component2
-// ]
